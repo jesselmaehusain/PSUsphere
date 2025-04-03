1386485fbc6bd3a937b3e41b8e524489e478f282
Jessel Mae Husain <202180287@psu.palawan.edu.ph>
2025-04-03 13:24:52 +0800

    modify allowed host

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l(tor(v6#$9abo076kuf5r^%umd$@aihy$diecr4=w@%v8r_)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
