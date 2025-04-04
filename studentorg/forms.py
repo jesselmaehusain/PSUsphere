606b430efcd61ab12581f783bdcfa44313033432
Jessel Mae Husain <202180287@psu.palawan.edu.ph>
2025-04-04 20:07:10 +0800

    Remove unwanted files and directories

--- studentorg/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm # type: ignore
-from django import forms # type: ignore
-from .models import Organization # type: ignore
-
-class OrganizationForm(ModelForm):
-    class Meta:
-        model = Organization
-        fields = "__all__"
--- studentorg/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-from studentorg.models import Organization
-
-class HomePageView(ListView):
-    model = Organization
-    context_object_name = 'home'
-    template_name = "home.html"
