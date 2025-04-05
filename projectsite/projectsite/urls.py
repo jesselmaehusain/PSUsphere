4fc0a11becd39e5a1552389043e95d4a9c9e5262
Jessel Mae Husain <202180287@psu.palawan.edu.ph>
2025-04-06 00:55:06 +0800

    Modified Student html in templates is working

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,13 +1,32 @@
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
-from studentorg import views
+from studentorg.views import (
+    HomePageView,
+    OrganizationList,
+    OrganizationCreateView,
+    OrganizationUpdateView,
+    OrganizationDeleteView,
+    StudentList,
+    StudentCreateView,
+    StudentUpdateView,
+    StudentDeleteView,
+)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
-    path('organization_list', OrganizationList.as_view(), name='organization-list'),
-    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
-    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
-    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    path('admin/', admin.site.urls),
+
+    # Home page
+    path('', HomePageView.as_view(), name='home'),
+
+    # Organization URLs
+    path('organization_list/', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization-delete'),
+
+    # Student list
+    path('student_list/', StudentList.as_view(), name='student_list'),
+    path('student_list/add/', StudentCreateView.as_view(), name='student-add'),
+    path('student_list/<int:pk>/', StudentUpdateView.as_view(), name='student-update'),
+    path('student_list/<int:pk>/delete/', StudentDeleteView.as_view(), name='student-delete'),
 ]
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
-from studentorg.forms import OrganizationForm
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from studentorg.models import Organization, Student
+from studentorg.forms import OrganizationForm, StudentForm
 
-class HomePageView(ListView):
-    model = Organization
-    context_object_name = 'home'
+class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['organizations'] = Organization.objects.all()
+        context['students'] = Student.objects.all()
+        return context
 
 class OrganizationList(ListView):
     model = Organization
-    context_object_name = 'organization'
+    context_object_name = 'organizations'
     template_name = 'org_list.html'
     paginate_by = 5
 
@@ -32,3 +35,28 @@ class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
+
+class StudentList(ListView):
+    model = Student
+    context_object_name = 'students'
+    template_name = 'student_list.html'
+    paginate_by = 5
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student_add.html'
+    success_url = reverse_lazy('student-list')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'student_edit.html'
+    success_url = reverse_lazy('student-list')
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'student_del.html'
+    success_url = reverse_lazy('student-list')
+    
+
