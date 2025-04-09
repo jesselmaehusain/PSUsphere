9fddb4b0f365fa7aa035569b7944591d837bc47f
Jessel Mae Husain <202180287@psu.palawan.edu.ph>
2025-04-09 13:54:39 +0800

    Delete duplicate files

--- projectsite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from studentorg.views import HomePageView
-from studentorg import views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
-]
